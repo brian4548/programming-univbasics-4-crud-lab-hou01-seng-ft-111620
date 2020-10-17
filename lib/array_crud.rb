f3d9f62a3494df1f213a22a886bcb831073553d6
Brian Rhodes <brianr4548@gmail.com>
2020-10-17 02:50:59 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -32,7 +32,7 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  color_choices = ["red", "blue", "green","purple"]
+  color_choices = ["wow", "blue", "green","purple"]
   color_choices[0]
 end
 
